156c75a072417e5dafba158143d932033e4264ba
Kobchok Chuawong <kobchok.chuawong@gmail.com>
2025-12-10 01:08:54 +0700

    visualize data

--- src/Graph/Inflation.py
+++ src/Graph/Inflation.py
@@ -14,13 +14,15 @@ inflation_year = FilteredInflation['Year'].unique()
 
 print(f"Inflation Countries: {country_list}")
 # print(f"Inflation Averages: {Afghanistan_inflation_avg}")
-
+plt.figure(figsize=(12,6))
 for i in range(len(country_list)):
     inflation_avg = FilteredInflation['Avg.Inflation'].loc[FilteredInflation['Country'] == country_list[i]]
     plt.plot(inflation_year, inflation_avg, label=country_list[i])
+
 
 plt.legend()
 plt.xlabel('Year')
 plt.ylabel('Average Inflation') 
 plt.title('Average Inflation of Afghanistan Over Years')
-plt.show()
+plt.tight_layout()
+plt.savefig('results/average_inflation_over_years.png', dpi=300, bbox_inches='tight')
